d4f4ece7d161200f681c2ed69dddfd4e98c733cc
tech4commun <pedaltriangle@gmail.com>
2026-07-23 20:26:54 +0530

    Add expense deletion handler

--- backend/delete.py
+++ backend/delete.py
@@ -1,8 +1,19 @@
 import json, boto3, os
+
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['TABLE_NAME'])
+
+CORS_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type'
+}
 
 def lambda_handler(event, context):
     expense_id = event['pathParameters']['expenseId']
     table.delete_item(Key={'expenseId': expense_id})
-    return {'statusCode': 200, 'body': json.dumps({'message': 'Deleted'})}
+    return {
+        'statusCode': 200,
+        'headers': CORS_HEADERS,
+        'body': json.dumps({'message': 'Deleted'})
+    }
